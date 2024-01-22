343a08e688fe8a6cbcb88ddbf76021a47f00bf33
Yuri Istomin <nekocoder.pw@gmail.com>
2024-01-22 14:27:16 +0200

    Init project

---
+++ README.md
@@ -0,0 +1,15 @@
+# Word counter
+### Word counter count unique words from a file
+
+## Building app
+
+```shell
+cmake -B build -G Ninja
+cmake --build build
+```
+
+## Running app
+
+```shell
+./build/word_counter
+```
---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+//===--- main.cpp ---------------------------------------------------------===//
+//
+// Copyright (c) 2024 Yuri Istomin
+//
+// Part of the NekoCdr's "word-counter" project
+// Licensed under the Apache License, Version 2.0 (the "License").
+// See http://www.apache.org/licenses/LICENSE-2.0 for license information.
+//
+//===----------------------------------------------------------------------===//
+
+// NOLINTNEXTLINE
+int main()
+{
+    return 0;
+}
